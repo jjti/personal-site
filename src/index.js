fd79058b0a77a9c690adb37f4730c4273837881b
Joshua Timmons <timmons.j@husky.neu.edu>
2017-10-01 22:22:55 -0400

    sync script for aws
    -- no more prism
    ++ hide the sync script file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Blog, Publications, Writing } from "./pages";
-
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import { Blog, Publications, Writing } from "./pages";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import "./zenburn.css";
+import "./index.css";
 
 class TrackPageView extends React.Component {
 	componentWillMount() {
