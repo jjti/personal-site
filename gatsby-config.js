12d12d81c2ae3000438296539cfbe6e15699647e
Joshua Timmons <timmons.j@husky.neu.edu>
2017-11-18 15:13:20 -0500

    fixes to cache invalidation and changed target links from markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
         plugins: [
           `gatsby-remark-smartypants`,
           "gatsby-remark-copy-linked-files",
+          "gatsby-remark-external-links",
           {
             resolve: `gatsby-remark-images`,
             options: {
--- sync.js
+++ sync.js
@@ -7,22 +7,24 @@ const buildDir = path.resolve("./public");
 console.log(buildDir);
 
 child_process.exec(
-	`aws s3 sync ${buildDir} s3://www.joshuatimmons.com --delete --cache-control max-age=604800,public`,
-	(error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		} else if (stdout) {
-			child_process.exec(
-				`aws cloudfront create-invalidation --distribution-id E1MRNWEDPJJGK --paths /index.html /404.html`,
-				(errorcf, stdoutcf, stderrcf) => {
-					console.log(`error-cf: ${error}`);
-					console.log(`stdout-cf: ${stdoutcf}`);
-					console.log(`stderr-cf: ${stderrcf}`);
-				}
-			);
-		} else {
-			console.log(`stderr: ${stderr}`);
-		}
-	}
+  `aws s3 sync ${
+    buildDir
+  } s3://www.joshuatimmons.com --acl public-read --sse --delete --cache-control max-age=604800,public`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    } else if (stdout) {
+      child_process.exec(
+        `aws cloudfront create-invalidation --distribution-id E1MRNWEDPJJGK --paths "/*"`,
+        (errorcf, stdoutcf, stderrcf) => {
+          console.log(`error-cf: ${error}`);
+          console.log(`stdout-cf: ${stdoutcf}`);
+          console.log(`stderr-cf: ${stderrcf}`);
+        }
+      );
+    } else {
+      console.log(`stderr: ${stderr}`);
+    }
+  }
 );
