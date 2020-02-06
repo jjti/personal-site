6dc9a7b1db9fd1773e8b0b4d70479c2e48fc1670
JJTimmons <joshua.timmons1@gmail.com>
2020-02-06 13:42:17 -0500

    Update photo to smaller JPG

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -2,4 +2,6 @@ import React from "react";
 
 import face from "../files/face.jpg";
 
-export default () => <img style={{ maxWidth: 300, marginLeft: 10 }} src={face} />;
+export default () => (
+  <img style={{ maxWidth: 300, marginLeft: 10 }} src={face} />
+);
